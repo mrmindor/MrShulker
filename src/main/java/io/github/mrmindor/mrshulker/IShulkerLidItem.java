5078e3cdc3bb221b51c08e62902165a75d95cff2
mr.mindor <mr.mindor@gmail.com>
2025-05-18 19:03:50 -0500

    bump to Alpha Added backward compatibility for migrating from ShulkerPlus mod. (tested between ShulkerPlus 1.21 and MrShulker 1.21.5) Cleaned Up Anvil Menu behavior:     Now should present consistent behavior between adding/removing lid items, and renaming shulkers so player can change the shulker name without losing the lid item, and vice versa. Cleaned up component tags:     If lid item is removed, custom data tag is removed entirely.     If a container is used for a lid item, the lid item is added with an empty inventory.

--- src/main/java/io/github/mrmindor/mrshulker/IShulkerLidItem.java
+++ src/main/java/io/github/mrmindor/mrshulker/IShulkerLidItem.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 public interface IShulkerLidItem {
     Optional<ItemStack> getLidItem();
-    void setLidItem(ItemStack stack);
     static IShulkerLidItem from(Object object) {return (IShulkerLidItem) object;}
 }
